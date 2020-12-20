29729509afcf36a8dcc702e7309973e35e5fe7fc
Heman Robinson <heman.robinson@gmail.com>
2020-12-20 14:33:59 -0500

    zoom limits

--- src/Plot.js
+++ src/Plot.js
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect, useState }  from 'react';
-import { Button, Slider } from '@material-ui/core';
+import React, { useRef, useEffect }  from 'react';
+import { Slider } from '@material-ui/core';
 import * as d3 from 'd3';
 import Data from './Data';
 import './Plot.css';
@@ -10,14 +10,16 @@ const Plot = ( props ) => {
     // Create state.
     let { dataSet } = props,
         data = Data.getValues( dataSet ),
-        [ xRange, setXRange ] = useState([ 0, 1 ]),
-        [ yRange, setYRange ] = useState([ 0, 1 ]);
+        xMin0 = d3.min( data, d => d[ 2 ]),
+        xMax0 = d3.max( data, d => d[ 2 ]),
+        yMin0 = d3.min( data, d => d[ 1 ]),
+        yMax0 = d3.max( data, d => d[ 1 ]);
     
     // Create reference and scales.
     const padding = 20, marginAxis = 50, buttonSize = 30, height = 400, width = 400;
     let ref = useRef(),
-        xScale = d3.scaleLinear().domain([ d3.min( data, d => d[ 2 ]), d3.max( data, d => d[ 2 ])]).range([ marginAxis + padding, width - padding ]),
-        yScale = d3.scaleLinear().domain([ d3.min( data, d => d[ 1 ]), d3.max( data, d => d[ 1 ])]).range([ height - marginAxis - padding, padding ]);
+        xScale = d3.scaleLinear().domain([ xMin0, xMax0 ]).range([ marginAxis + padding, width - padding ]),
+        yScale = d3.scaleLinear().domain([ yMin0, yMax0 ]).range([ height - marginAxis - padding, padding ]);
     
     // Set hook to draw on mounting, or on any other lifecycle update.
     useEffect(() => {
@@ -26,21 +28,22 @@ const Plot = ( props ) => {
     
     // Zoom in or out.
     let onZoom = ( isIn ) => {
-            let xDomain = xScale.domain(),
-                xMin = xDomain[ 0 ],
-                xMax = xDomain[ 1 ],
-                xRange = xMax - xMin,
-                yDomain = yScale.domain(),
-                yMin = yDomain[ 0 ],
-                yMax = yDomain[ 1 ],
-                yRange = yMax - yMin;
+            const d = 8, f = ( d - 1 ) / ( 2 * d );
+            let xDomain = xScale.domain(), xMin = xDomain[ 0 ], xMax = xDomain[ 1 ], xRange = xMax - xMin,
+                yDomain = yScale.domain(), yMin = yDomain[ 0 ], yMax = yDomain[ 1 ], yRange = yMax - yMin;
             if( isIn ) {
-                xScale.domain([ xMin + xRange / 4, xMax - xRange / 4 ]);
-                yScale.domain([ yMin + yRange / 4, yMax - yRange / 4 ]);
+                xMin = Math.min( xMin0 + ( xMax0 - xMin0 ) * f, xMin + xRange / d );
+                xMax = Math.max( xMax0 - ( xMax0 - xMin0 ) * f, xMax - xRange / d );
+                yMin = Math.min( yMin0 + ( yMax0 - yMin0 ) * f, yMin + yRange / d );
+                yMax = Math.max( yMax0 - ( yMax0 - yMin0 ) * f, yMax - yRange / d );
             } else {
-                xScale.domain([ xMin - xRange / 2, xMax + xRange / 2 ]);
-                yScale.domain([ yMin - yRange / 2, yMax + yRange / 2 ]);
+                xMin = Math.max( xMin0, xMin - xRange / ( d - 2 ));
+                xMax = Math.min( xMax0, xMax + xRange / ( d - 2 ));
+                yMin = Math.max( yMin0, yMin - yRange / ( d - 2 ));
+                yMax = Math.min( yMax0, yMax + yRange / ( d - 2 ));
             }
+            xScale.domain([ xMin, xMax ]);
+            yScale.domain([ yMin, yMax ]);
             Plot.draw( height, width, marginAxis, padding, ref, xScale, yScale, dataSet, 100 );
         },
         onZoomIn  = () => { onZoom( true  ); },
@@ -77,7 +80,7 @@ Plot.draw = ( height, width, marginAxis, padding, ref, xScale, yScale, dataSet, 
     svg.append( "g" )
         .attr( "class", "axis" )
         .attr( "transform", "translate( 0, " + ( height - marginAxis ) + " )" )
-        .call( d3.axisBottom( xScale ).ticks( 2.5 ).tickFormat(( x ) => { return x.toFixed( 0 )}));
+        .call( d3.axisBottom( xScale ).ticks( 3 ).tickFormat(( x ) => { return x.toFixed( 1 )}));
     svg.append( "text" )
         .attr( "transform", "translate( " + ( width / 2 ) + " ," + ( height - padding ) + ")" )
         .style( "text-anchor", "middle" )
@@ -87,7 +90,7 @@ Plot.draw = ( height, width, marginAxis, padding, ref, xScale, yScale, dataSet, 
     svg.append( "g" )
         .attr( "class", "axis" )
         .attr( "transform", "translate( " + marginAxis + ", 0 )" )
-        .call( d3.axisLeft( yScale ).ticks( 3 ).tickFormat(( x ) => { return x.toFixed( 0 )}));
+        .call( d3.axisLeft( yScale ).ticks( 3 ).tickFormat(( x ) => { return x.toFixed( 1 )}));
     svg.append( "text" )
         .attr( "transform", "rotate( -90 )" )
         .attr( "x", -height / 2 )
