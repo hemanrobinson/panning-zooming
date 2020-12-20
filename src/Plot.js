930140406b2394af546e4335a0ee31f1cd7113b2
Heman Robinson <heman.robinson@gmail.com>
2020-12-20 13:01:07 -0500

    onClick

--- src/Plot.js
+++ src/Plot.js
@@ -8,26 +8,49 @@ import './Plot.css';
 const Plot = ( props ) => {
     
     // Create state.
-    const [ xRange, setXRange ] = useState([ 0, 1 ]);
+    let { dataSet } = props,
+        data = Data.getValues( dataSet ),
+        [ xRange, setXRange ] = useState([ 0, 1 ]),
+        [ yRange, setYRange ] = useState([ 0, 1 ]);
     
     // Create reference and scales.
     const padding = 20, marginAxis = 50, buttonSize = 30, height = 400, width = 400;
     let ref = useRef(),
-        { dataSet } = props,
-        data = Data.getValues( dataSet ),
         xScale = d3.scaleLinear().domain([ d3.min( data, d => d[ 2 ]), d3.max( data, d => d[ 2 ])]).range([ marginAxis + padding, width - padding ]),
         yScale = d3.scaleLinear().domain([ d3.min( data, d => d[ 1 ]), d3.max( data, d => d[ 1 ])]).range([ height - marginAxis - padding, padding ]);
     
-    // Hook to draw on mounting, or on any other lifecycle update.
+    // Set hook to draw on mounting, or on any other lifecycle update.
     useEffect(() => {
         Plot.draw( height, width, marginAxis, padding, ref, xScale, yScale, dataSet, 100 );
     });
     
+    // Zoom in or out.
+    let onZoom = ( isIn ) => {
+            let xDomain = xScale.domain(),
+                xMin = xDomain[ 0 ],
+                xMax = xDomain[ 1 ],
+                xRange = xMax - xMin,
+                yDomain = yScale.domain(),
+                yMin = yDomain[ 0 ],
+                yMax = yDomain[ 1 ],
+                yRange = yMax - yMin;
+            if( isIn ) {
+                xScale.domain([ xMin + xRange / 4, xMax - xRange / 4 ]);
+                yScale.domain([ yMin + yRange / 4, yMax - yRange / 4 ]);
+            } else {
+                xScale.domain([ xMin - xRange / 2, xMax + xRange / 2 ]);
+                yScale.domain([ yMin - yRange / 2, yMax + yRange / 2 ]);
+            }
+            Plot.draw( height, width, marginAxis, padding, ref, xScale, yScale, dataSet, 100 );
+        },
+        onZoomIn  = () => { onZoom( true  ); },
+        onZoomOut = () => { onZoom( false ); };
+    
     // Return the component.
     return <div style={{width: width, height: height}} className="parent">
             <svg width={width} height={height} ref={ref}></svg>
-            <input type="button" value="+" style={{ width: buttonSize, height: buttonSize, top: ( height + 1 - buttonSize ), left: 1 }} />
-            <input type="button" value="-" style={{ width: buttonSize, height: buttonSize, top: ( height + 1 - buttonSize ), left: 1 + buttonSize }} />
+            <input type="button" value="+" onClick={onZoomIn } style={{ width: buttonSize, height: buttonSize, top: ( height + 1 - buttonSize ), left: 1 }} />
+            <input type="button" value="-" onClick={onZoomOut} style={{ width: buttonSize, height: buttonSize, top: ( height + 1 - buttonSize ), left: 1 + buttonSize }} />
         </div>;
 };
     
