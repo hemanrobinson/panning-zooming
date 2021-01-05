159eb0411d9c6a86f17d7b1659249c9583e1f339
Heman Robinson <heman.robinson@gmail.com>
2021-01-05 11:04:47 -0500

    heat map

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     return (
         <div className="Column">
             <div className="Description">
-                <h1>Applying Shneiderman's Mantra<br/>to Aggregate Graphs</h1>
+                <h1>Applying Shneiderman's Mantra<br/>to Zoom Aggregate Graphs</h1>
                 <p>
                 Shneiderman taught us all to <a href="http://www.cs.umd.edu/~ben/papers/Shneiderman1996eyes.pdf">"Overview first, zoom and filter, then details-on-demand"</a>.
                 </p>
@@ -51,7 +51,7 @@ const App = () => {
             </div>
             <div className="Description">
                 <p>
-                This user interface extends readily to multiple dimensions.  To minimize distraction, controls can be displayed only when needed, upon a mouse hover or click event.  The heatmap below demonstrates both features.
+                This user interface extends readily to multiple dimensions.  To minimize distraction from the data display, controls can be displayed only when needed, upon a mouse hover or click event.  The heatmap below demonstrates both features.
                 </p>
             </div>
             <div className="Graph">
@@ -60,10 +60,16 @@ const App = () => {
             <div className="Description">
                 <h2>Why This User Interface</h2>
                 <p>
-                Three user interfaces were tested.
+                There are three places one can place a user interface for zooming:  on the data display, on the axis, or somewhere outside the graph.
                 </p>
                 <p>
-                Display primary controls on hover or click, so as not to distract from data display.
+                We hypothesized that the third position would be easiest to learn, because the control is more visible and can be labeled, e.g. a slider labeled "Zoom Factor (0-1):", or a numeric entry field labeled "Bin Width:".  However, this position requires additional screen real estate, which may be expensive; or placement in a secondary dialog, which is less efficient for the user.  Shneiderman's Mantra suggests that all four operations (overview, zoom, filter, details on demand) should be easy and efficient for the user to explore their data.
+                </p>
+                <p>
+                Shneiderman also said that direct manipulation is superior to indirect manipulation.  This argues for the first position.  This user interface is implemented in JMP(r) software, using a hand cursor to manipulate histogram bars.  We hypothesized that this position would be most difficult to learn, because manipulating a data display directly is not a familiar action, and a hand cursor displays only a few pixels.
+                </p>
+                <p>
+                These three user interfaces were tested.
                 </p>
                 <p>
                 Display secondary controls in a dialog.
--- src/Heatmap.js
+++ src/Heatmap.js
@@ -10,25 +10,33 @@ const Heatmap = ( props ) => {
     // Initialization.
     const width = 600,
         height = 400,
-        padding = { top: 20, right: 20, bottom: 0, left: 20 },
-        margin = { top: 0, right: 0, bottom: 80, left: 50 };
+        padding = { top: 20, right: 20, bottom: 0, left: 0 },
+        margin = { top: 0, right: 0, bottom: 50, left: 80 };
     let ref = useRef(),
         { dataSet } = props,
-        xLabel = Data.getColumnNames( dataSet )[ 0 ],
-        yLabel = "Frequency",
+        xLabel = Data.getColumnNames( dataSet )[ 1 ],
+        yLabel = Data.getColumnNames( dataSet )[ 0 ],
         data = Data.getValues( dataSet ),
-        xDomain0,
+        xDomain0 = [ d3.min( data, d => d[ 1 ]), d3.max( data, d => d[ 1 ])],
         yDomain0,
         xScale,
         yScale,
         downLocation = { x: 0, y: 0, xDomain: [], yDomain: [], isX: false, isY: false, isMin: false, isMax: false },
-        bars,
         histogram,
-        bins;
+        bins,
+        tiles;
         
     // Get the X scale.
-    const [ xDomain, setXDomain ] = useState([]);
-    xScale = d3.scaleBand().domain( xDomain ).range([ margin.left + padding.left, width - margin.right - padding.right ]).padding( 0.2 );
+    const [ xDomain, setXDomain ] = useState( xDomain0 );
+    xScale = d3.scaleLinear().domain( xDomain ).range([ margin.left + padding.left, width - margin.right - padding.right ]);
+        
+    // Get the unique values.
+    let values = data.map( datum => datum[ 0 ]);
+    yDomain0 = values.filter(( item, index ) => ( values.indexOf( item ) === index ));
+        
+    // Get the Y scale.
+    const [ yDomain, setYDomain ] = useState( yDomain0 );
+    yScale = d3.scaleBand().domain( yDomain ).range([ height - margin.bottom - padding.bottom, margin.top + padding.top ]);
     
     // Assign the X group factor.
     const [ xGroup, setXGroup ] = useState( 0 );
@@ -36,43 +44,41 @@ const Heatmap = ( props ) => {
         setXDomain( xScale.domain());
         setXGroup( value );
     };
+    
+    // Assign the Y group factor.
+    const [ yGroup, setYGroup ] = useState( 0 );
+    let onYGroup = ( event, value ) => {
+        setYDomain( yScale.domain());
+        setYGroup( value );
+    };
 
-    // Calculate the histogram bins.
+    // Calculate the bins.
     histogram = d3.histogram()
-        .value( d => d[ 2 ])
+        .value( d => d[ 1 ])
         .domain( xDomain0 )
         .thresholds( Math.round( 8 + Math.exp( 5 * xGroup )));
     bins = histogram( data );
-
-    // Get the Y scale.
-    yDomain0 = [ 0, d3.max( bins, d => d.length )];
-    yScale = d3.scaleLinear()
-        .domain( yDomain0 )
-        .range([ height - margin.bottom - padding.bottom, margin.top + padding.top ]);
-
-    // Calculate the bars.
-    bars = Array.from( d3.rollup( data, v => v.length, d => d[ 0 ]));
-    bars.sort(( a, b ) => ( b[ 1 ] - a[ 1 ]));
     
-    // Combine bars if requested.
-    let n = Math.round( xGroup * bars.length );
-    if( 0 < n ) {
-        let total = 0;
-        for( let i = 0; ( i < n ); i++ ) {
-            total += bars[ bars.length - i - 1 ][ 1 ];
+    // Count the number of values in each bin.
+    tiles = [];
+    bins.forEach(( bin ) => {
+        let t = [];
+        for( let i = 0; ( i < yDomain0.length ); i++ ) {
+            t[ i ] = { y: yDomain[ i ], count: 0 };
         }
-        bars.splice( bars.length - n, n );
-        bars.push([ "Other", total ]);
-    }
-    
-    // Set the X domain.
-    xDomain0 = bars.map( x => x[ 0 ]);
-    xScale.domain( xDomain0 );
+        bin.forEach(( b ) => {
+            let k = yDomain0.indexOf( b[ 0 ]);
+            t[ k ].count++;
+            t[ k ].x0 = bin.x0;
+            t[ k ].x1 = bin.x1;
+        })
+        tiles = tiles.concat( t );
+    });
         
     // Zoom in two dimensions.
     let onZoom2D = ( isIn ) => {
         Graph.onZoom2D( isIn, xScale, yScale, xDomain0, yDomain0 );
-        Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bars );
+        Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bins, tiles );
     };
     
     // Zoom in one dimension.
@@ -82,37 +88,42 @@ const Heatmap = ( props ) => {
     onMouseUp = ( event ) => {
         if( downLocation.isX || downLocation.isY ) {
             Graph.onMouseUp( event, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, downLocation );
-            Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bars );
+            Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bins, tiles );
         }
     };
     
     // Set hook to draw on mounting, or on any other lifecycle update.
     useEffect(() => {
-        Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bars );
+        Heatmap.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bins, tiles );
     });
     
     // Return the component.
     return <Graph width={width} height={height} margin={margin} padding={padding}
-        onZoom={onZoom2D} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onXGroup={onXGroup} ref={ref} />
+        onZoom={onZoom2D} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onXGroup={onXGroup} onYGroup={onYGroup} ref={ref} />
 };
     
 // Draws the Bar Chart.
-Heatmap.draw = ( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bars ) => {
+Heatmap.draw = ( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel, bins, tiles ) => {
     
     // Initialization.
     const svg = d3.select( ref.current );
     svg.selectAll( "*" ).remove();
+    
+    // Get the color scale.
+    let colorScale = d3.scaleLinear().domain([ 0, d3.max( tiles, t => t.count )]).range([ "#99bbdd", "#ff0000" ]);
 
-    // Draw the bars.
+    // Draw the tiles.
+    let w = xScale( tiles[ 0 ].x1 ) - xScale( tiles[ 0 ].x0 ) - 1,
+        h = yScale.bandwidth() - 1;
     svg.selectAll( "rect" )
-        .data( bars )
+        .data( tiles )
         .enter()
         .append( "rect" )
-        .attr( "x", ( d ) => xScale( d[ 0 ]))
-        .attr( "y", ( d ) => yScale( d[ 1 ]))
-        .attr( "width", xScale.bandwidth())
-        .attr( "height", ( d ) => (( xScale.domain().indexOf( d[ 0 ]) >= 0 ) ? Math.max( 0, height - yScale( d[ 1 ])) : 0 ))
-        .style( "fill", "#99bbdd" );
+        .attr( "x", ( d ) => xScale( d.x0 ))
+        .attr( "y", ( d ) => yScale( d.y ) + 1 )
+        .attr( "width", w )
+        .attr( "height", h )
+        .style( "fill", ( d ) => colorScale( d.count ));
         
     // Draw the axes and scroll bars.
     Graph.draw( ref, height, width, margin, padding, xScale, yScale, xDomain0, yDomain0, xLabel, yLabel );
