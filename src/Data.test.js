a8f3013d24e1f7f0c56cb4be86f68681a3a35352
Heman Robinson <heman.robinson@gmail.com>
2023-01-04 09:57:15 -0500

    update data test

--- src/Data.test.js
+++ src/Data.test.js
@@ -7,18 +7,19 @@ it( "invokes Data function", () => {
 it( "returns column names", () => {
     expect( Data.getColumnNames()).toEqual([ "Species", "Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)" ]);
     expect( Data.getColumnNames( "Iris" )).toEqual([ "Species", "Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)" ]);
-    expect( Data.getColumnNames( "Cytometry" )).toEqual([ "Cluster", "Prin 1", "Prin 2", "ForSc", "SideSc", "CD3", "CD8", "CD4", "MCB", "Distance" ]);
-    expect( Data.getColumnNames( "Food" )).toEqual([ "Food", "Calories", "Fat", "Carbohydrates", "Protein" ]);
-    expect( Data.getColumnNames( "Sales" )).toEqual([ "Model", "20Q4 (thousands)" ]);
-    expect( Data.getColumnNames( "S&P 500" )).toEqual([ "Year", "S&P 500 Index" ]);
+    expect( Data.getColumnNames( "Normal" )).toEqual([ "Random Normal, Bimodal" ]);
+    expect( Data.getColumnNames( "Penguins" )).toEqual([ "Species", "Island", "Bill Length (mm)", "Bill Depth (mm)", "Flipper Length (mm)", "Body Mass (g)", "Sex" ]);
+    expect( Data.getColumnNames( "Sales" )).toEqual([ "Model", "Cars Sold, 2022 Q1" ]);
+    expect( Data.getColumnNames( "Stocks" )).toEqual([ "Year", "S&P 500 Index" ]);
+    expect( Data.getColumnNames( "Trends" )).toEqual([ "Week Number", "Week", "Google Trends 2020", "Interest" ]);
 });
 
 it( "returns values", () => {
     expect( Data.getValues().length ).toBe( 150 );
     expect( Data.getValues( "Iris" ).length ).toBe( 150 );
-    expect( Data.getValues( "Cytometry" ).length ).toBe( 500 );
-    expect( Data.getValues( "Food" ).length ).toBe( 303 );
-    expect( Data.getValues( "Sales" ).length ).toBe( 359 );
-    expect( Data.getValues( "S&P 500" ).length ).toBe( 153 );
+    expect( Data.getValues( "Normal" ).length ).toBe( 500 );
+    expect( Data.getValues( "Penguins" ).length ).toBe( 342 );
+    expect( Data.getValues( "Sales" ).length ).toBe( 288 );
+    expect( Data.getValues( "Stocks" ).length ).toBe( 153 );
+    expect( Data.getValues( "Trends" ).length ).toBe( 1113 );
 });
-
